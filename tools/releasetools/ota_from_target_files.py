11a17193fd896dbe9346f2008014fd04bac4fa10
calebcabob <calphonic@gmail.com>
2017-09-10 20:07:01 -0400

    Fix backup script
    Change-Id: If61222f314e4ba858afe34a1cf6f46435ba7ec44

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -543,12 +543,9 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   device_specific.FullOTA_PostValidate()
 
   if OPTIONS.backuptool:
-    script.ShowProgress(0.02, 10)
-    if block_based:
-      script.Mount("/system")
-    script.RunBackup("restore")
-    if block_based:
-      script.Unmount("/system")
+    script.Mount("/system")
+    script.RunBackup("backup")
+    script.Unmount("/system")
 
   script.Print(" ")
   script.Print("Flashing Kernel..")
